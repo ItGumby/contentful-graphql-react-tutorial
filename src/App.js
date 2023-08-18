f025fb19e4e64ead9cbaa341072e24dfd969f889
Brian Street <brian.c.street@gmail.com>
2023-08-18 16:51:44 -0600

    step 1: react counter example, contentful fetch with useState, useEffect. also secrets out of source into external .env file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const query = `
+query {
+  author: componentAuthor(id: "2NKz77emgdgCCePcbxHwzR") {
+    name
+    internalName
+  }
+}
+`;
+
+const {
+  REACT_APP_SPACE_ID,
+  REACT_APP_ACCESS_TOKEN,
+} = process.env;
+
 function App() {
+  // temp counter distraction to highlight useState, useEffect
+  let [count, setCount] = useState(0); // [curVal, setter] = useState(initVal)
+  let [data, setData] = useState(null);
+
+  useEffect(() => {
+    window.fetch(`https://graphql.contentful.com/content/v1/spaces/${REACT_APP_SPACE_ID}?access_token=${REACT_APP_ACCESS_TOKEN}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({query}),
+    }).then(response => response.json())
+    .then(json => setData(json.data));
+  }, []); // run once on DOM injection
+
+  if (!data) return <span>Loading...</span>;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p>counter: {count}</p>
+        <button onClick={() => setCount(count + 1)}>increment</button>
+
+        <h1>{data.author.name}</h1>
       </header>
     </div>
   );
