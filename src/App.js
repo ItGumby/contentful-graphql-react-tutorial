324d8a62953a1e32064cf42209af18222e55e7da
Brian Street <brian.c.street@gmail.com>
2023-08-21 19:17:39 -0600

    step 12: conditional query with GraphQL @include directive

--- src/App.js
+++ src/App.js
@@ -5,19 +5,15 @@ import Bookmarks from './components/bookmark.js';
 import './App.css';
 
 const query = `
-query($isPreview: Boolean=false) {
-  author: componentAuthor(id: "2NKz77emgdgCCePcbxHwzR", preview: $isPreview) {
-    name
-    socialLinkedIn
-    socialGithub
-    avatar {
-      title
-      url
-    }
-    bio {
-      json
-    }
+query($isPreview: Boolean=false){
+  author: componentAuthor(id: "2NKz77emgdgCCePcbxHwzR") {
+    ...personFields
   }
+  authorPreview: componentAuthor(id: "2NKz77emgdgCCePcbxHwzR", preview: $isPreview)
+    @include(if: $isPreview) {
+    ...personFields
+  }
+
   allBookmarks: bookmarkCollection {
     items {
       ...bookmarkFields
@@ -35,6 +31,15 @@ query($isPreview: Boolean=false) {
       }
     }
   }
+  componentAuthorCollection(
+    where: {OR: [{name_contains: "Brian"}, {name_contains: "Ann"}]}
+  ) {
+    total
+    items {
+      sys { id }
+      name
+    }
+  }
 }
 
 fragment bookmarkFields on Bookmark {
@@ -46,29 +51,42 @@ fragment bookmarkFields on Bookmark {
     items { title }
   }
 }
-`;
-
-const IS_PREVIEW = true;
+fragment personFields on ComponentAuthor {
+  name
+  socialLinkedIn
+  socialGithub
+  avatar {
+    title
+    url
+  }
+  bio { json }
+}`;
 
 function App() {
   // temp counter distraction to highlight useState, useEffect
+  let [isPreview, setIsPreview] = useState(false);
   let [count, setCount] = useState(0); // [curVal, setter] = useState(initVal)
 
-  let {data, errors} = useContentful(query, IS_PREVIEW); // NOTE: error "Cannot destructure property" if 'let {data} ...'
+  let {data, errors} = useContentful(query, isPreview); // NOTE: error "Cannot destructure property" if 'let {data} ...'
   if (errors) {
     return <ul style={{color: "red"}}><li>ERRORS:</li><li>{errors.map(err => err.message).join('</li><li>')}</li></ul>
   }
   if (!data) return <div>Loading...</div>;
 
-  const { allBookmarks, favTagCollection, author} = data;
+  const { allBookmarks, favTagCollection, author, authorPreview } = data;
   const favoriteTag = favTagCollection.items[0];
 
   return (
     <div className="App">
       <header className="App-header">
+        <label>
+          <input type="checkbox" checked={isPreview} onChange={() => setIsPreview(!isPreview)}></input>
+          Show Preview
+        </label>
         <p>counter: {count}</p>
         <button onClick={() => setCount(count + 1)}>increment</button>
 
+        {authorPreview ? <Person person={authorPreview} /> : ""}
         <Person person={author} />
         <Bookmarks heading="Favorite" list={favoriteTag.linkedFrom.bookmarkCollection.items} />
         <Bookmarks heading="All bookmarks" list={allBookmarks.items} />
